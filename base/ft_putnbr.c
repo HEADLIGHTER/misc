00f0d459f81f8dbce84907351b0e33385e087453
Benny Bellatrix <Headlighterone@gmail.com>
2021-04-21 02:45:39 +0300

    37firstpush

---
+++ base/ft_putnbr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbellatr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/21 02:28:56 by bbellatr          #+#    #+#             */
+/*   Updated: 2021/04/21 02:28:56 by bbellatr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_puchar (char c)
+{
+	write(1, c, 1);
+}
+
+void ft_putnbr(int nb)
+{
+	in (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(nb * -1);
+	}
+	else if (nb < 10)
+		ft_putchar((char)(nb + '0'));
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar((char)(nb % 10 + '0'));
+	}
+}
